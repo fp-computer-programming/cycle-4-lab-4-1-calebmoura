41faf3e16292276f63c5095a2d1353e24a2f8b8f
calebmoura <144838461+calebmoura@users.noreply.github.com>
2023-10-23 04:45:32 -0400

    Add files via upload

---
+++ lab_4-1.py
@@ -0,0 +1,15 @@
+# Author: caleb moura
+
+# Prompt user to enter their first name as variable
+first_name = input("Enter your first name: ")
+
+# Prompt user to enter their last name as variable
+last_name = input("Enter your last name: ")
+
+# Create new variable for user's full name
+full_name = first_name + " " + last_name
+
+# Print result
+print("The user's full name is:", full_name)
+
+# Result: everything works, when both individual input variables are put in the full name is printed
